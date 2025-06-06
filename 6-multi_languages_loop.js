7a803c2bf1d51f93eba9d27ad8ceb494c46fd715
Leonard Chepkwony <leonchepkwony@gmail.com>
2025-06-06 10:49:06 +0300

    6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,12 +1,3 @@
-// The first line: “C is fun”
-// The second line: “Python is cool”
-// The third line: “JavaScript is amazing”
-// You must use console.log(...) to print all output
-// You are not allowed to use var
-// You are not allowed to use any if/else statement
-// You can use only one console.log
-// You must use a loop (while, for, etc.)
-
 languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 for(let i=0; i < languages.length; i++){
